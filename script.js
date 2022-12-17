3414c184ef84537acff3595ea60046449f6e3584
Audrey-Albuquerque <audreydev.br@gmail.com>
2022-12-17 10:37:29 -0300

    :ok_woman: Renderização Dom e innerHTML, adição de content e for, remoção do console.log e das divs e seus conteúdos no HTML

--- script.js
+++ script.js
@@ -63,21 +63,43 @@ function carregaDicionario() {
         },
     };
 
-       for (var minibio in biografias) {
-       
-                console.log((minibio +=
-             " " +
-              "Nome: " +
-               biografias[minibio].nome +
-                " " +
-                 "Minibio: " +
-                  biografias[minibio].descricao +
-                  " " + 
-                  "Citação: "+
-                  biografias[minibio].citacao)
-        );
-    }
+    var content = document.getElementById("content");
 
+  for (var bio in biografias) {
+
+    content.innerHTML +=
+
+      '<div class="card">' +
+
+      '<img src="' +
+
+      biografias[bio].imagem +
+
+      '"/>' +
+
+      "<details>" +
+
+      "<summary>" +
+
+      biografias[bio].nome +
+
+      "</summary>" +
+
+      "<p>" +
+
+      biografias[bio].descricao +
+
+      "</p>" +
+
+      "<blockquote><q>" +
+
+      biografias[bio].citacao +
+
+      "</q></blockquote>" +
+
+      "</details></div>";
+
+  }
 }
 
 carregaDicionario();
